dc69d5851d8f9fb1d0d2b736f0e2920b716dc812
VyankateshDande <vyankateshdande.vd@gmail.com>
2023-08-08 19:02:39 +0530

    Fixed cooldown

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # cooldown
- A simple cooldown checker
+A simple cooldown checker.
+
+## Usage
+Use `createCooldown()` function to create a cooldown tracker. 
+
+Then use the `tick()` function to know whether or not the function is on cooldown or not.
+
+## Example
+`limit` is the amount of times a cooldown can be used in specified `period`.
+
+Note: `period` must be in seconds
+```js
+let limit = 2;
+let period = 15; // default: 60
+
+let commandCooldown = createCooldown(limit, period);
+// In this example, cooldown is 2 times in 15 seconds.
+
+console.log(commandCooldown.tick());
+// expected output: true
+
+console.log(commandCooldown.tick());
+// expected output: true
+
+console.log(commandCooldown.tick());
+// expected output: false. 
+
+// After 15 seconds
+console.log(commandCooldown.tick());
+// expected output: true
+```
--- index.ts
+++ index.ts
@@ -1,18 +1,20 @@
-interface cooldown {
+export interface cooldown {
     limit: number;      // Number of times a function can be used per minute
+    period: number;     // The period for which the cooldown works, in seconds. Default is 60s, ie, 1 minute.
     strikes: number[];  // Timestamps of times a function has been used
     tick: Function;     // Checks if a function is useable at this moment
 }
 
-export function createCooldown(limitPerMin: number): cooldown {
+export function createCooldown(limitPerMin: number, period: number = 60): cooldown {
     let tracker: cooldown = {
         limit: limitPerMin,
+        period: period *1000,
         strikes: [],
         tick: () => {
             for (let strike in tracker.strikes) {
                 if (
-                    (Date.now() - tracker.strikes[strike]) > 10000
-                ) tracker.strikes.splice(Number(strike), Number(strike));
+                    (Date.now() - tracker.strikes[strike]) > tracker.period
+                ) tracker.strikes = tracker.strikes.splice(Number(strike), Number(strike));
             };
 
             if (tracker.strikes.length + 1 > tracker.limit) return false;
